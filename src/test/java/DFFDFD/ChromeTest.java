14b2197a52240090bbe9b1aaf2fe897da3794301
Serega <kamserg1608@gmail.com>
2019-04-11 14:30:52 +0300

    jenkins

--- src/test/java/DFFDFD/ChromeTest.java
+++ src/test/java/DFFDFD/ChromeTest.java
@@ -8,6 +8,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 
@@ -15,32 +17,33 @@ public class ChromeTest {
     private static ChromeDriverService service;
     private WebDriver driver;
 
-    @BeforeClass
-    public static void createAndStartService() throws IOException {
-        service = new ChromeDriverService.Builder()
-                .usingPort(11107)
-                .build();
-        service.start();
-    }
-
-    @AfterClass
-    public static void createAndStopService() {
-        service.stop();
-    }
-
-    @BeforeMethod
-    public void createDriver() {
-        driver = new RemoteWebDriver(service.getUrl(),
-                DesiredCapabilities.chrome());
-    }
-
-    @AfterMethod
-    public void quitDriver() {
-        driver.quit();
-    }
+//    @BeforeClass
+//    public static void createAndStartService() throws IOException {
+//        service = new ChromeDriverService.Builder()
+//                .usingPort(11107)
+//                .build();
+//        service.start();
+//    }
+//
+//    @AfterClass
+//    public static void createAndStopService() {
+//        service.stop();
+//    }
+//
+//    @BeforeMethod
+//    public void createDriver() {
+//        driver = new RemoteWebDriver(service.getUrl(),
+//                DesiredCapabilities.chrome());
+//    }
+//
+//    @AfterMethod
+//    public void quitDriver() {
+//        driver.quit();
+//    }
     @Test
-    public void firstTest123(){
-//       WebDriver driver = new ChromeDriver();
+    public void firstTest123() throws MalformedURLException {
+       DesiredCapabilities capability = DesiredCapabilities.chrome();
+       WebDriver  driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
        driver.manage().window().maximize();
        driver.navigate().to("http://localhost:8080/armada");
